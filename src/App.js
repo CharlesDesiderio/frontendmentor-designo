3079c1405825b7a6bf93253e94c51341796d6139
Charles Desiderio <cjdesi@gmail.com>
2021-10-05 10:51:05 -0500

    Added assets and design styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+
+<h1>Header 1</h1>
+<h2>Header 2</h2>
+<h3>Header 3</h3>
+
     </div>
-  );
+  )
 }
 
-export default App;
+/*
+
+layout:
+ALL PAGES: HEADER, CTA, FOOTER
+
+*/
+
+export default App
